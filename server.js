ac3ee04b7635a1065221e5bb4ab61fb6d5c48e72
yubin <yubin618034@gmail.com>
2018-06-03 21:25:36 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (error) => {
+    if (error) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'HomePage',
+    welcomeMessage: 'Welcome to visit my website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    code: 404,
+    description: 'Not Found'
+  });
+});
+app.listen(3000, () => {
+  console.log('Server is up on to port 3000');
+});
